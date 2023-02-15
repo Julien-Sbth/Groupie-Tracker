35dc2fcf4af60ec5f9154a4dc7eff8d2ca0e14a6
Julien Sabathé <https://ytrack.learn.ynov.com/git/sjulien/hangman-web.git>
2023-02-15 22:23:38 +0100

    [EDIT]

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
-}
+import (
+	"net/http"
+)
 
+func main() {
+	http.Handle("/front-end/", http.StripPrefix("/front-end", http.FileServer(http.Dir("front-end/css"))))
+	fs := http.FileServer(http.Dir("./front-end"))
+	http.Handle("/", fs)
+	http.ListenAndServe(":8080", nil)
+}

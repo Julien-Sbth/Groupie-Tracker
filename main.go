3ac46a113c7e49a6a97ab4f1c7be0229dfc02da3
Julien Sabathé <https://ytrack.learn.ynov.com/git/sjulien/hangman-web.git>
2023-02-20 21:57:39 +0100

    [ADD] [EDIT] file go and add info NWA

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"API/API"
+	"API/backend"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/front-end/", http.StripPrefix("/front-end", http.FileServer(http.Dir("front-end/css"))))
-	fs := http.FileServer(http.Dir("./front-end"))
-	http.Handle("/", fs)
-	http.ListenAndServe(":8080", nil)
+	// Serve static files from the "front-end" directory
+	fs := http.FileServer(http.Dir("front-end"))
+	http.Handle("/front-end/", http.StripPrefix("/front-end/", fs))
+
+	// Set up API endpoints
+	http.HandleFunc("/", backend.HandleTest)
+	http.HandleFunc("/dates", API.HandleDates)
+	http.HandleFunc("/artist", API.HandleArtists)
+	http.HandleFunc("/data", API.HandleData)
+	http.HandleFunc("/location", API.HandleLocations)
+	http.HandleFunc("/info", API.HandleInfo)
+	http.HandleFunc("/relation", API.HandleRelations)
+
+	http.HandleFunc("/nwa", backend.HandleInfoNWA)
+	http.HandleFunc("/shakur", backend.HandleInfoShakur)
+	http.HandleFunc("/notorious", backend.HandleInfoNotorious)
+	http.HandleFunc("/lamar", backend.HandleInfoLamar)
+	http.HandleFunc("/warren", backend.HandleInfoWarren)
+	http.HandleFunc("/daft", backend.HandleInfoDaftPunk)
+
+	// Start server
+	fmt.Println("Server started on port :8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }

85c1d373c17780229c94581a52fd225083341323
Julien Sabathé <https://ytrack.learn.ynov.com/git/sjulien/hangman-web.git>
2023-02-28 16:15:38 +0100

    [MEGA EDIT] API in main and delete file inutile

---
+++ go.mod
@@ -0,0 +1,9 @@
+module API
+
+require github.com/gorilla/mux v1.8.0
+
+replace API v1.0.0 => ./API/
+
+replace backend v1.0.0 => ./backend/
+
+go 1.19
--- main.go
+++ main.go
@@ -1,80 +1,58 @@
 package main
 
 import (
-	"API/API"
-	"API/backend"
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type Artist struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Albums   string `json:"albums"`
+	Tracks   string `json:"tracks"`
+	Self     string `json:"self"`
+	ImageURL string `json:"image"`
+}
+
 func main() {
-	// Serve static files from the "front-end" directory
-	http.Handle("/front-end/", http.StripPrefix("/front-end/", http.FileServer(http.Dir("front-end/css"))))
-	HandleTest := http.FileServer(http.Dir("./front-end"))
+	r := mux.NewRouter()
 
-	// Set up API endpoints
-	http.Handle("/", HandleTest)
-	http.HandleFunc("/search", API.HandleSearch)
-	http.HandleFunc("/date", API.HandleDates)
+	r.HandleFunc("/", homeHandler).Methods("GET")
 
-	http.HandleFunc("/nwa", backend.HandleInfoNWA)
-	http.HandleFunc("/infoJSONAW", backend.HandleInfoJSONNWA)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
 
-	http.HandleFunc("/infoJSONAPS", backend.HandleInfoJSONNaps)
-	http.HandleFunc("/naps", backend.HandleInfoNaps)
-
-	http.HandleFunc("/sch", backend.HandleInfoSCH)
-	http.HandleFunc("/infoJSONSCH", backend.HandleInfoJSONSCH)
-
-	http.HandleFunc("/jul", backend.HandleInfoJul)
-	http.HandleFunc("/infoJSONJul", backend.HandleInfoJSONJul)
-
-	http.HandleFunc("/dion", backend.HandleInfoDion)
-	http.HandleFunc("/infoJSONDion", backend.HandleInfoJSONDion)
-
-	http.HandleFunc("/maes", backend.HandleInfoMaes)
-	http.HandleFunc("/infoJSONMaes", backend.HandleInfoJSONMaes)
-
-	http.HandleFunc("/lamar", backend.HandleInfoLamar)
-	http.HandleFunc("/infoJSONLamar", backend.HandleInfoJSONLamar)
-
-	http.HandleFunc("/shakur", backend.HandleInfoShakur)
-	http.HandleFunc("/infoJSONShakur", backend.HandleInfoJSONShakur)
-
-	http.HandleFunc("/kaaris", backend.HandleInfoKaaris)
-	http.HandleFunc("/infoJSONKaaris", backend.HandleInfoJSONKaaris)
-
-	http.HandleFunc("/warren", backend.HandleInfoWarren)
-	http.HandleFunc("/infoJSONWarren", backend.HandleInfoJSONWarren)
-
-	http.HandleFunc("/infoJSONDaftPunk", backend.HandleInfoJSONDaftPunk)
-	http.HandleFunc("/daft", backend.HandleInfoDaftPunk)
-
-	http.HandleFunc("/infoJSONSnoopDog", backend.HandleInfoJSONSnoopDog)
-	http.HandleFunc("/snoop", backend.HandleInfoSnoopDog)
-
-	http.HandleFunc("/soprano", backend.HandleInfoSoprano)
-	http.HandleFunc("/infoJSONSoprano", backend.HandleInfoJSONSoprano)
-
-	http.HandleFunc("/halliday", backend.HandleInfoHalliday)
-	http.HandleFunc("/infoJSONHalliday", backend.HandleInfoJSONHalliday)
-
-	http.HandleFunc("/big", backend.HandleInfoBigflOli)
-	http.HandleFunc("/infoJSONBigflolie", backend.HandleInfoJSONBigflolie)
-
-	http.HandleFunc("/infoJSONEminem", backend.HandleInfoJSONEminem)
-	http.HandleFunc("/eminem", backend.HandleInfoEminem)
-
-	http.HandleFunc("/infoJSONBiggies", backend.HandleInfoJSONBig)
-	http.HandleFunc("/notorious", backend.HandleInfoNotorious)
-
-	http.HandleFunc("/tentation", backend.HandleInfoXXXTentation)
-	http.HandleFunc("/infoJSONXXX", backend.HandleInfoJSONXXXTentation)
-
-	// Start server
-	fmt.Println("Server started on port :8080")
-	err := http.ListenAndServe("localhost:8080", nil)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-		panic(err)
+		fmt.Println("Erreur lors de la requête HTTP :", err)
+		return
 	}
+	defer resp.Body.Close()
+
+	var artists []Artist
+	err = json.NewDecoder(resp.Body).Decode(&artists)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture de la réponse JSON :", err)
+		return
+	}
+
+	// Ajouter un ID unique pour chaque artiste
+	for i := range artists {
+		artists[i].ID = i + 1
+	}
+
+	tmpl, err := template.ParseFiles("artist.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tmpl.Execute(w, artists)
 }

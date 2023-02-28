05f4b7c349a12c8800afba9de4fd7345bd8e81d4
Louis Sellier <louis.sellier33@gmail.com>
2023-02-28 17:12:11 +0100

    [EDIT] Résolution de bugs

--- go.mod
+++ go.mod
@@ -1,12 +1,10 @@
 module API
 
-<<<<<<< HEAD
-=======
-require github.com/gorilla/mux v1.8.0
-
->>>>>>> master
 replace API v1.0.0 => ./API/
 
 replace backend v1.0.0 => ./backend/
 
+require github.com/gorilla/mux v1.8.0
+
 go 1.19
+
--- main.go
+++ main.go
@@ -1,103 +1,16 @@
 package main
 
 import (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	"API/API"
-	"API/backend"
-	"fmt"
->>>>>>> master
-=======
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
->>>>>>> master
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-<<<<<<< HEAD
-func main() {
-<<<<<<< HEAD
-	http.Handle("/front-end/", http.StripPrefix("/front-end", http.FileServer(http.Dir("front-end/css"))))
-	fs := http.FileServer(http.Dir("./front-end"))
-	http.Handle("/", fs)
-	http.ListenAndServe(":8080", nil)
-=======
-	// Serve static files from the "front-end" directory
-	http.Handle("/front-end/", http.StripPrefix("/front-end/", http.FileServer(http.Dir("front-end/css"))))
-	HandleTest := http.FileServer(http.Dir("./front-end"))
-
-	// Set up API endpoints
-	http.Handle("/", HandleTest)
-	http.HandleFunc("/search", API.HandleSearch)
-	http.HandleFunc("/date", API.HandleDates)
-
-	http.HandleFunc("/nwa", backend.HandleInfoNWA)
-	http.HandleFunc("/infoJSONAW", backend.HandleInfoJSONNWA)
-
-	http.HandleFunc("/infoJSONAPS", backend.HandleInfoJSONNaps)
-	http.HandleFunc("/naps", backend.HandleInfoNaps)
-
-	http.HandleFunc("/sch", backend.HandleInfoSCH)
-	http.HandleFunc("/infoJSONSCH", backend.HandleInfoJSONSCH)
-
-	http.HandleFunc("/jul", backend.HandleInfoJul)
-	http.HandleFunc("/infoJSONJul", backend.HandleInfoJSONJul)
-
-	http.HandleFunc("/dion", backend.HandleInfoDion)
-	http.HandleFunc("/infoJSONDion", backend.HandleInfoJSONDion)
-
-	http.HandleFunc("/maes", backend.HandleInfoMaes)
-	http.HandleFunc("/infoJSONMaes", backend.HandleInfoJSONMaes)
-
-	http.HandleFunc("/lamar", backend.HandleInfoLamar)
-	http.HandleFunc("/infoJSONLamar", backend.HandleInfoJSONLamar)
-
-	http.HandleFunc("/shakur", backend.HandleInfoShakur)
-	http.HandleFunc("/infoJSONShakur", backend.HandleInfoJSONShakur)
-
-	http.HandleFunc("/kaaris", backend.HandleInfoKaaris)
-	http.HandleFunc("/infoJSONKaaris", backend.HandleInfoJSONKaaris)
-
-	http.HandleFunc("/warren", backend.HandleInfoWarren)
-	http.HandleFunc("/infoJSONWarren", backend.HandleInfoJSONWarren)
-
-	http.HandleFunc("/infoJSONDaftPunk", backend.HandleInfoJSONDaftPunk)
-	http.HandleFunc("/daft", backend.HandleInfoDaftPunk)
-
-	http.HandleFunc("/infoJSONSnoopDog", backend.HandleInfoJSONSnoopDog)
-	http.HandleFunc("/snoop", backend.HandleInfoSnoopDog)
-
-	http.HandleFunc("/soprano", backend.HandleInfoSoprano)
-	http.HandleFunc("/infoJSONSoprano", backend.HandleInfoJSONSoprano)
-
-	http.HandleFunc("/halliday", backend.HandleInfoHalliday)
-	http.HandleFunc("/infoJSONHalliday", backend.HandleInfoJSONHalliday)
-
-	http.HandleFunc("/big", backend.HandleInfoBigflOli)
-	http.HandleFunc("/infoJSONBigflolie", backend.HandleInfoJSONBigflolie)
-
-	http.HandleFunc("/infoJSONEminem", backend.HandleInfoJSONEminem)
-	http.HandleFunc("/eminem", backend.HandleInfoEminem)
-
-	http.HandleFunc("/infoJSONBiggies", backend.HandleInfoJSONBig)
-	http.HandleFunc("/notorious", backend.HandleInfoNotorious)
-
-	http.HandleFunc("/tentation", backend.HandleInfoXXXTentation)
-	http.HandleFunc("/infoJSONXXX", backend.HandleInfoJSONXXXTentation)
-
-	// Start server
-	fmt.Println("Server started on port :8080")
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		panic(err)
-	}
->>>>>>> master
-=======
 type Artist struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
@@ -115,13 +28,18 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
 		fmt.Println("Erreur lors de la requête HTTP :", err)
 		return
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
 
 	var artists []Artist
 	err = json.NewDecoder(resp.Body).Decode(&artists)
@@ -135,13 +53,12 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		artists[i].ID = i + 1
 	}
 
-	tmpl, err := template.ParseFiles("front-end/accueil.html")
+	tmpl, err := template.ParseFiles("artist.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tmpl.Execute(w, artists)
->>>>>>> master
+	_ = tmpl.Execute(w, artists)
 }

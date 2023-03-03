84c50f5db785e0a77392d9aa2c3bd63ef9bcc981
Julien Sabathé <https://ytrack.learn.ynov.com/git/sjulien/hangman-web.git>
2023-03-03 17:10:09 +0100

    [ADD] Main

--- main.go
+++ main.go
@@ -23,31 +23,23 @@ type Artist struct {
 	ConcertDate  string   `json:"ConcertDate"`
 	Dates        []string `json:"dates"`
 }
-
 type Dates struct {
 	ID int `json:"id"`
 }
 
 func main() {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/", homeHandler).Methods("GET")
-	r.HandleFunc("/index/{id}", artistDetailsHandler).Methods("GET")
+	r.HandleFunc("/artist/{id}", artistDetailsHandler).Methods("GET")
 	r.HandleFunc("/date/{id}", datesHandler).Methods("GET")
-
 	fs := http.FileServer(http.Dir("front-end/css"))
 	cssHandler := http.StripPrefix("/css/", fs)
-
 	r.PathPrefix("/css/").Handler(cssHandler)
-
 	fs = http.FileServer(http.Dir("front-end"))
 	fileHandler := http.StripPrefix("/front-end/", fs)
-
 	r.PathPrefix("/front-end/").Handler(fileHandler)
-
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
@@ -55,28 +47,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
-
 	var artists []Artist
 	err = json.NewDecoder(resp.Body).Decode(&artists)
 	if err != nil {
 		fmt.Println("Erreur lors de la lecture de la réponse JSON :", err)
 		return
 	}
-
 	for i := range artists {
 		artists[i].ID = i + 1
 	}
-
 	tmpl, err := template.ParseFiles("front-end/accueil.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tmpl.Execute(w, artists)
 }
-
 func artistDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, ok := vars["id"]
@@ -84,16 +71,13 @@ func artistDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Missing artist ID", http.StatusBadRequest)
 		return
 	}
-
 	resp, err := http.Get(fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%s", id))
 	if err != nil {
 		fmt.Println("Erreur lors de la requête HTTP :", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	defer resp.Body.Close()
-
 	var artist Artist
 	err = json.NewDecoder(resp.Body).Decode(&artist)
 	if err != nil {
@@ -101,19 +85,15 @@ func artistDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	tmpl, err := template.ParseFiles("front-end/index.html")
+	tmpl, err := template.ParseFiles("front-end/artiste.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	artist.ConcertDate = fmt.Sprintf("/date/{id}%s", id)
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tmpl.Execute(w, artist)
 }
-
 func datesHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, ok := vars["id"]
@@ -128,7 +108,6 @@ func datesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
-
 	var dates []Dates
 	err = json.NewDecoder(resp.Body).Decode(&dates)
 	if err != nil {
@@ -136,13 +115,11 @@ func datesHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	tmpl, err := template.ParseFiles("front-end/artiste.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tmpl.Execute(w, dates)
 }

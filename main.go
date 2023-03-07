2f0c295dfb58184c7b87ecba1f49998cc71f7c19
Julien Sabathé <https://ytrack.learn.ynov.com/git/sjulien/hangman-web.git>
2023-03-07 01:12:27 +0100

    [EDIT] API Concert

--- main.go
+++ main.go
@@ -10,18 +10,24 @@ import (
 )
 
 type Artist struct {
-	ID           int         `json:"id"`
-	Name         string      `json:"name"`
-	Albums       string      `json:"albums"`
-	Tracks       string      `json:"tracks"`
-	Self         string      `json:"self"`
-	ImageURL     string      `json:"image"`
-	CreationDate int64       `json:"creationDate"`
-	Members      []string    `json:"members"`
-	FirstAlbum   string      `json:"FirstAlbum"`
-	Locations    string      `json:"locations"`
-	ConcertDate  string      `json:"ConcertDate"`
-	Dates        interface{} `json:"dates"`
+	ID             int         `json:"id"`
+	Name           string      `json:"name"`
+	Albums         string      `json:"albums"`
+	Tracks         string      `json:"tracks"`
+	Self           string      `json:"self"`
+	ImageURL       string      `json:"image"`
+	CreationDate   int64       `json:"creationDate"`
+	Members        []string    `json:"members"`
+	FirstAlbum     string      `json:"FirstAlbum"`
+	Locations      interface{} `json:"locations"`
+	DatesLocations interface{} `json:"dateslocations"`
+	ConcertDate    string      `json:"ConcertDate"`
+	Dates          interface{} `json:"dates"`
+	Latitude       float64     `json:"lat"`
+	Longitude      float64     `json:"long"`
+	Country        string      `json:"country"`
+	City           string      `json:"city"`
+	ConcertID      string      `json:"ConcertID"`
 }
 
 func main() {
@@ -29,6 +35,8 @@ func main() {
 	r.HandleFunc("/", homeHandler).Methods("GET")
 	r.HandleFunc("/index/{id}", artistDetailsHandler).Methods("GET")
 	r.HandleFunc("/date/", datesHandler).Methods("GET")
+	r.HandleFunc("/date/", getLocations).Methods("GET")
+	r.HandleFunc("/date/", GetRelation).Methods("GET")
 	fs := http.FileServer(http.Dir("front-end/css"))
 	cssHandler := http.StripPrefix("/css/", fs)
 	r.PathPrefix("/css/").Handler(cssHandler)
@@ -61,9 +69,10 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tmpl.Execute(w, artists)
 }
-func getArtistWithDates(id string) (*Artist, error) {
+func getArtistWithDatesLocationsAndRelations(id string) (*Artist, error) {
 	artistURL := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%s", id)
-	datesURL := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/dates/%s", id)
+	datesLocationsURL := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/dates/%s", id)
+	relationsURL := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/relations/%s", id)
 
 	artistResp, err := http.Get(artistURL)
 	if err != nil {
@@ -77,23 +86,62 @@ func getArtistWithDates(id string) (*Artist, error) {
 		return nil, fmt.Errorf("failed to decode artist response: %v", err)
 	}
 
-	datesResp, err := http.Get(datesURL)
+	datesLocationsResp, err := http.Get(datesLocationsURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get dates: %v", err)
+		return nil, fmt.Errorf("failed to get dates and locations: %v", err)
 	}
-	defer datesResp.Body.Close()
+	defer datesLocationsResp.Body.Close()
 
-	var dates interface{}
-	err = json.NewDecoder(datesResp.Body).Decode(&dates)
+	var datesLocations []DateLocation
+	err = json.NewDecoder(datesLocationsResp.Body).Decode(&datesLocations)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode dates response: %v", err)
+		return nil, fmt.Errorf("failed to decode dates and locations response: %v", err)
 	}
 
-	artist.Dates = dates
+	relationsResp, err := http.Get(relationsURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get relations: %v", err)
+	}
+	defer relationsResp.Body.Close()
+
+	var relations interface{}
+	err = json.NewDecoder(relationsResp.Body).Decode(&relations)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode relations response: %v", err)
+	}
+
+	for i := range relations {
+		for j := range datesLocations {
+			if relations[i].ConcertID == datesLocations[j].Location.ID {
+				relations[i].Date = datesLocations[j].Date
+				relations[i].Location = datesLocations[j].Location
+			}
+		}
+	}
+
+	artist.DatesLocations = relations
+	artist.Dates = datesLocations
 
 	return &artist, nil
 }
 
+func getLocations(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		http.Error(w, "Missing artist ID", http.StatusBadRequest)
+		return
+	}
+
+	_, err := getArtistWithDatesLocationsAndRelations(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// call locationsHandler
+	http.Redirect(w, r, fmt.Sprintf("/locations/%s", id), http.StatusSeeOther)
+}
 func datesHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, ok := vars["id"]
@@ -102,21 +150,32 @@ func datesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	artist, err := getArtistWithDates(id)
+	_, err := getArtistWithDatesLocationsAndRelations(id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	tmpl, err := template.ParseFiles("front-end/artiste.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tmpl.Execute(w, artist)
+	// call locationsHandler
+	http.Redirect(w, r, fmt.Sprintf("/locations/%s", id), http.StatusSeeOther)
 }
+func GetRelation(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		http.Error(w, "Missing artist ID", http.StatusBadRequest)
+		return
+	}
 
+	_, err := getArtistWithDatesLocationsAndRelations(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// call locationsHandler
+	http.Redirect(w, r, fmt.Sprintf("/locations/%s", id), http.StatusSeeOther)
+}
 func artistDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, ok := vars["id"]
@@ -125,7 +184,7 @@ func artistDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	artist, err := getArtistWithDates(id)
+	artist, err := getArtistWithDatesLocationsAndRelations(id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
